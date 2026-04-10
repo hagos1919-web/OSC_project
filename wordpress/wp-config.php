27cd0ead467469a9aba0c5e28a73a9b8a9a865c6
amanuelnega79 <amanuelnega79@gmail.com>
2026-04-09 20:33:55 -0900

    save all changes

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'osc_project' );
+define( 'DB_NAME', 'wp_project' );
 
 /** Database username */
-define( 'DB_USER', 'student' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'password123' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
